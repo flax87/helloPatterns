3a1ad377be1669b49f3de11f75439ad960931ff8
Alexej Shevchenko <65944505+flax87@users.noreply.github.com>
2020-06-17 09:53:52 +0300

    Update Figure.java

--- src/com/company/Figure.java
+++ src/com/company/Figure.java
@@ -1,5 +1,20 @@
+/*
+ * @author  Oleksii Shevchenko KNUTE 1m
+ *
+ * Programming patterns. Factory, Builder
+ *
+ *@version 16.06.20
+ * Develop for your class
+ *
+ * 1. Factory.
+ * 2. Abstract factory.
+ * 3. Create a class Student  - 25 fields.
+ * 4. Create a builder for the class Student
+ */
+
 package com.company;
 
-public enum Figure {
+//enum
+public enum Figure {            
     Triangle, Coub;
 }
