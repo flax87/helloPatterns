86d3a2ad8c87e7e0a496fc653ae00ba9d9daa8d4
Alexej Shevchenko <65944505+flax87@users.noreply.github.com>
2020-06-17 09:57:08 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,17 @@
+/*
+ * @author  Oleksii Shevchenko KNUTE 1m
+ *
+ * Programming patterns. Factory, Builder
+ *
+ *@version 16.06.20
+ * Develop for your class
+ *
+ * 1. Factory.
+ * 2. Abstract factory.
+ * 3. Create a class Student  - 25 fields.
+ * 4. Create a builder for the class Student
+ */
+
 package com.company;
 
 public class Main {
@@ -5,14 +19,14 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Triangle triangle1=new Triangle(3,4,5);
+        Triangle triangle1=new Triangle(3,4,5); 
         System.out.println(triangle1.toString());
 
 
-
+//2. print Abstract Factory
         IGeometry triangle2 = AbstractFactory.create(3,6, Figure.Triangle);
         System.out.println(triangle2.toString());
-
+//1. print Factory
         Triangle triangle3 = TriangleFactory.create(3,7, 7);
         System.out.println(triangle3.toString());
 
