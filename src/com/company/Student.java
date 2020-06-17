d65a95954601952ecfcf14465825b88eada11828
Alexej Shevchenko <65944505+flax87@users.noreply.github.com>
2020-06-17 10:00:45 +0300

    Update Student.java

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,3 +1,17 @@
+/*
+ * @author  Oleksii Shevchenko KNUTE 1m
+ *
+ * Programming patterns. Factory, Builder
+ *
+ *@version 16.06.20
+ * Develop for your class
+ *
+ * 1. Factory.
+ * 2. Abstract factory.
+ * 3. Create a class Student  - 25 fields.
+ * 4. Create a builder for the class Student
+ */
+
 package com.company;
 
 public class Student {
