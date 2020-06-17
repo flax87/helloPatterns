c9f2bd31434b6981f2a695c1c3a4b11b5711f8f1
Alexej Shevchenko <65944505+flax87@users.noreply.github.com>
2020-06-17 09:54:53 +0300

    Update IGeometry.java

--- src/com/company/IGeometry.java
+++ src/com/company/IGeometry.java
@@ -1,7 +1,21 @@
+/*
+ * @author  Oleksii Shevchenko KNUTE 1m
+ *
+ * Programming patterns. Factory, Builder
+ *
+ *@version 16.06.20
+ * Develop for your class
+ *
+ * 1. Factory.
+ * 2. Abstract factory.
+ * 3. Create a class Student  - 25 fields.
+ * 4. Create a builder for the class Student
+ */
+
 package com.company;
 
-public interface IGeometry {
+public interface IGeometry {      //create interface
 
-    double GetArea ();
+    double GetArea ();             
     double GetPerimeter();
 }
