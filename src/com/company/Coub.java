41d13c1d4a583a12aca9f97f0d515b3657c011e9
Alexej Shevchenko <65944505+flax87@users.noreply.github.com>
2020-06-17 09:52:48 +0300

    Update Coub.java

--- src/com/company/Coub.java
+++ src/com/company/Coub.java
@@ -1,20 +1,32 @@
+/*
+ * @author  Oleksii Shevchenko KNUTE 1m
+ *
+ * Programming patterns. Factory, Builder
+ *
+ *@version 16.06.20
+ * Develop for your class
+ *
+ * 1. Factory.
+ * 2. Abstract factory.
+ * 3. Create a class Student  - 25 fields.
+ * 4. Create a builder for the class Student
+ */
+
+
 package com.company;
 
 import java.util.Objects;
 
-public class Coub implements IGeometry {
-    double side;
+public class Coub implements IGeometry {     
+    double side;                                                //one side for the cube
 
 
-
-
-
-// constrktr
+// constructor
     public Coub(double side) {
         this.side = side;
     }
 
-//gtr & strs
+//get & set
     public double getSide() {
         return side;
     }
@@ -24,7 +36,7 @@ public class Coub implements IGeometry {
     }
 
 
-
+//equals & hashCode
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -38,21 +50,21 @@ public class Coub implements IGeometry {
         return Objects.hash(side);
     }
 
-
+//toString
     @Override
     public String toString() {
         return "Coub{" +
                 "side=" + side +
                 '}';
     }
-
+//IGeometry
     @Override
-    public double GetArea() {
+    public double GetArea() {                               //determine the area
         return (side*side)*6;
     }
 
     @Override
-    public double GetPerimeter() {
+    public double GetPerimeter() {                          //determine the perimeter
         return side*12;
     }
 }
