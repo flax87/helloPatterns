b7b87069298819df38f038a91190e1716e3e8ce7
Alexej Shevchenko <65944505+flax87@users.noreply.github.com>
2020-06-17 09:43:03 +0300

    Update AbstractFactory.java

--- src/com/company/AbstractFactory.java
+++ src/com/company/AbstractFactory.java
@@ -1,4 +1,19 @@
+/*
+ * @author  Oleksii Shevchenko KNUTE 1m
+ *
+ * Programming patterns. Factory, Builder
+ *
+ *
+ * Develop for your class
+ *
+ * 1. Factory.
+ * 2. Abstract factory.
+ * 3. Create a class Student  - 25 fields.
+ * 4. Create a builder for the class Student
+ */
 package com.company;
+
+//Create Abstract Factory
 
 public class AbstractFactory {
 
